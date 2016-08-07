6cd53a9ba918bf7232d4cd0418058471cf621570
marvin evins <maevins@cps.edu>
2016-08-07 09:13:12 -0500

    Initial Commit

---
+++ youtube/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  youtube
+//
+//  Created by marvin evins on 8/7/16.
+//  Copyright © 2016 marvin evins. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
